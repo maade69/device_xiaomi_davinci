cc73b2540a8e2fe600396efd5019aa3d845f4528
maade69 <p1nchp0d@bk.ru>
2020-10-25 20:49:40 +0300

    davinci: add support for faceunlock

--- octavi_davinci.mk
+++ octavi_davinci.mk
@@ -24,6 +24,7 @@ $(call inherit-product, device/xiaomi/davinci/device.mk)
 # Inherit some common OctaviOS stuff.
 $(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
